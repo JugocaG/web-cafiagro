cffd8753606a9759d834401e72c5e5ccb1dc3c4f
Jugoca <juangomcal@gmail.com>
2023-12-27 17:48:17 -0500

    Configuracion link de entrada

--- cafiagro.py
+++ cafiagro.py
@@ -9,7 +9,7 @@ excel_file_path = 'Datos.xlsx'
 
 
 # URL del sitio web
-url = 'https://gratis-vpfe.dian.gov.co/SupportDocuments/Adjustment'
+url = 'https://catalogo-vpfe.dian.gov.co/User/AuthToken?pk=10910094|26566160&rk=813013472&token=392084e8-e824-4f27-9d3c-b147174c89f8'
 
 # Función para leer datos de Excel
 def leer_datos_desde_excel(file_path):
@@ -28,25 +28,34 @@ driver = webdriver.Chrome()
 
 
 try:
+
+    driver.get(url)
+
+    # Abrir otra URL en la misma pestaña
+    # Abrir una nueva pestaña en blanco
+
+    # Realizar acciones en la segunda pestaña
+    # Puedes interactuar con elementos en la segunda pestaña aquí
     # Leer datos desde Excel
     datos_excel = leer_datos_desde_excel(excel_file_path)
 
     # Iterar sobre los datos y rellenar el formulario
-    for fila in datos_excel:
-        # Supongamos que el primer elemento en la fila es el nombre, el segundo es el correo, etc.
-        nombre, correo, telefono = fila
+    # for fila in datos_excel:
+    #     # Supongamos que el primer elemento en la fila es el nombre, el segundo es el correo, etc.
+    #     nombre, correo, telefono = fila
 
-        # Localizar los campos del formulario por su nombre, id, u otros atributos
-        campo_nombre = driver.find_element(By.ID, 'OrderReference_ID')
+    #     # Localizar los campos del formulario por su nombre, id, u otros atributos
+    #     campo_nombre = driver.find_element(By.ID, 'OrderReference_ID')
         
 
-        # Rellenar los campos con datos de Excel
-        campo_nombre.send_keys(nombre)
+    #     # Rellenar los campos con datos de Excel
+    #     campo_nombre.send_keys(nombre)
         
-        input("Presiona Enter para salir...")
+    #     input("Presiona Enter para salir...")
 
-        # Puedes agregar más lógica según tus necesidades, como hacer clic en un botón de envío.
+    #     # Puedes agregar más lógica según tus necesidades, como hacer clic en un botón de envío.
 
 finally:
     # Cerrar el navegador al finalizar
+    input("Presiona Enter para salir...")
     driver.quit()
